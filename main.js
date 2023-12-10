b8bf039a8f17a4411e1206781e410e5ff3ff331c
reddy <reddysekhark90@gmail.com>
2023-12-10 12:44:18 +0530

    firstcommit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#menu-btn").click(function (e) {
+        e.stopPropagation(); // Prevents the click event from reaching the document body
+        $('nav .navigation ul').addClass('active');
+    });
+
+    $("#menu-close").click(function () {
+        $('nav .navigation ul').removeClass('active');
+    });
+
+    // Close the menu when clicking anywhere on the document body
+    $(document).on("click", function (e) {
+        if (!$(e.target).closest('nav .navigation').length) {
+            // Check if the click is outside the navigation menu
+            $('nav .navigation ul').removeClass('active');
+        }
+    });
+});
